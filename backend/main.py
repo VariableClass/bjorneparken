1431e97bcfcafb55241738ab73b659582c016dc5
Callum Veale <callum@callum-veales-imac.local>
2017-02-21 15:39:44 +0000

    Built framework for backend REST API.

---
+++ backend/main.py
@@ -0,0 +1,39 @@
+# [START imports]
+import endpoints
+from protorpc import message_types
+from protorpc import messages
+from protorpc import remote
+# [END imports]
+
+
+# [START messages]
+class AnimalRequest(messages.Message):
+    content = messages.StringField(1)
+
+
+class AnimalResponse(messages.Message):
+    content = messages.StringField(1)
+
+
+ANIMALS_RESOURCE = endpoints.ResourceContainer(
+    AnimalRequest)
+# [END messages]
+
+
+# [START api]
+@endpoints.api(name='bjorneparkappen', version='v1')
+class BjorneparkappenApi(remote.Service):
+
+    @endpoints.method(
+        ANIMALS_RESOURCE,
+        AnimalResponse,
+        path='animals',
+        http_method='GET',
+        name='animals.list')
+    def list_animals(self, request):
+        return AnimalResponse(content=request.content)
+# [END api]
+
+# [START api_server]
+api = endpoints.api_server([BjorneparkappenApi])
+# [END api_server]
