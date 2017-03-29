27d7d6b915f9f28a3764c4330be6a2716621a64e
Callum Veale <callum.veale@gmail.com>
2017-03-29 02:29:59 +0100

    Added phone/email actions on click.

--- android/Bjorneparken/app/src/main/java/com/callumveale/bjorneparken/fragments/HelpFragment.java
+++ android/Bjorneparken/app/src/main/java/com/callumveale/bjorneparken/fragments/HelpFragment.java
@@ -1,10 +1,13 @@
 package com.callumveale.bjorneparken.fragments;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 import com.callumveale.bjorneparken.R;
 
@@ -40,7 +43,33 @@ public class HelpFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_help, container, false);
+        View view = inflater.inflate(R.layout.fragment_help, container, false);
+
+        // Add telephone onclick listener
+        RelativeLayout telephone = (RelativeLayout) view.findViewById(R.id.help_tel);
+        telephone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:" + getString(R.string.help_tel)));
+                startActivity(intent);
+            }
+        });
+
+        RelativeLayout email = (RelativeLayout) view.findViewById(R.id.help_email);
+        email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("message/rfc822");
+                intent.putExtra(Intent.EXTRA_EMAIL, new String[] { getString(R.string.help_email) });
+                intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.email_subject));
+                Intent mailer = Intent.createChooser(intent, null);
+                startActivity(mailer);
+            }
+        });
+
+        return view;
     }
 
     //endregion Fragment Overridden Methods
